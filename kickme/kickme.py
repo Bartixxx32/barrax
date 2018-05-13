5caaadf7eaeee0cbafce9dc45b83e8dcadac6d0d
Bartixxx32 <mcbplay1@gmail.com>
2018-05-13 12:48:56 +0200

    Update kickme.py

--- kickme/kickme.py
+++ kickme/kickme.py
@@ -7,11 +7,4 @@ class Barrax:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def kickme(self, ctx):
-        """This cog will kick all who write this command"""
-        
-        await self.bot.say("User want to leave from this server :C")
-        await self.bot.say(ctx.message.author)
-def setup(bot):
-    bot.add_cog(Barrax(bot))
+s
